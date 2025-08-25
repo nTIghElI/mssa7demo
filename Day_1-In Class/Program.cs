d4aa97520803f4fc05fb6b0dcf448b294927ac95
your username <your email>
2025-08-25 13:23:55 -0400

    Initial Commit

---
+++ Day_1-In Class/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day_1_In_Class;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        //Exercise - Implement try-catch exception handling
+    }
+}
